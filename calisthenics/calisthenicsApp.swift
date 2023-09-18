0dd7ea19bae0b33fec86db354fadf32321b277cd
tayamirai9 <128257364+tayamirai9@users.noreply.github.com>
2023-09-18 14:24:54 +0100

    Initial Commit

---
+++ calisthenics/calisthenicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  calisthenicsApp.swift
+//  calisthenics
+//
+//  Created by Tayami Rai on 18/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct calisthenicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
